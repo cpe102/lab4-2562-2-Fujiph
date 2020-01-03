1819ab0bd01eeb279a2061f167997ef662dc2c1c
lab302-37 <lab302-37@labeng.local>
2020-01-03 15:26:22 +0700

    lab4_1v1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std ;
 
+
+float Operation( float x , float Y , float e = 2.71828) {
+    Y = 3 * pow(x,3) + 2 * e + pow(2,2*x+1) + pow((pow(x,2) + 1),0.5);
+    return Y;
+}
+
+int main () {
+    float y , X;
+    cout << "enter x = ";
+    cin >> X;
+    cout << "result y = " << Operation(X,y) ;
+}
