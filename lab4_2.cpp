385df5acd1d5feb5daa2b1ed69ecddde9b534070
lab302-37 <lab302-37@labeng.local>
2020-01-03 15:46:21 +0700

    lab4_2v1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
 //Write function findDistance() here
+float findDistance(float u , float a , float t) {
+  return u*t + 0.5 * a * pow(t,2);
+}
 
 int main(){
-
+  float U , A , T;
   //Calling findDistance() using test cases
+  cout << " enter u ";
+  cin >> U;
+  cout << " enter a ";
+  cin >> A;
+  cout << " enter t ";
+  cin >> T;
+  cout << " s is " << findDistance(U,A,T);
 
   return 0;
 }
